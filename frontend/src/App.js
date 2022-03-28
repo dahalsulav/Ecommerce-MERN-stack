2a55b9b096550f89d05c046f38815dced2ce56d8
dahalsulav <sulavd7@email.com>
2022-03-28 15:06:35 +0545

    created project using npx create-react-app

---
+++ frontend/src/App.js
@@ -0,0 +1,8 @@
+function App() { return (
+<div className="App">
+    <header className="App-header">
+        <a href="/">Ecommerce</a>
+    </header>
+    <main>List Products</main>
+</div>
+); } export default App;
